64415b13f8980f786c308f97f22284aa9d8126f6
yihui178 <18332585277@163.com>
2025-06-07 16:15:16 +0800

    Delete WMS.springboot1 directory

--- WMS.springboot1/src/main/java/com/example/web/tools/dto/IdInput.java
+++
@@ -1,9 +0,0 @@
-package com.example.web.tools.dto;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-
-@Data
-public class IdInput {
-    @JsonProperty("Id")
-    private Integer Id;
-}
